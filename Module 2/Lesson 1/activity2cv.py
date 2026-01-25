9d5e7c4a9201d34dd38a75b44d79a51c02f8b6fc
DevDaamodara <munagaladaamodara@gmail.com>
2026-01-25 14:17:22 +0400

    acp lesson 1

---
+++ Module 2/Lesson 1/activity2cv.py
@@ -0,0 +1,23 @@
+import cv2
+
+image = cv2.imread('exa.jpg')
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+resized_image = cv2.resize(gray_image, (224, 224))
+
+cv2.imshow('Processed Image', resized_image)
+
+key = cv2.waitKey(0)
+
+if key == ord('s'):
+    cv2.imwrite('gray_scale_image.jpg', resized_image)
+
+    print("Image saved as gray_scale_image.jpg")
+
+else:
+    print("Image not saved")
+
+cv2.destroyAllWindows()
+
+print(f"Processed Image Dimensions: {resized_image.shape}")
