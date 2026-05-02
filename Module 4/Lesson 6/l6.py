8cadd3fbfc4120cdf09f1f20644f6a89c7f904f2
DevDaamodara <munagaladaamodara@gmail.com>
2026-05-02 15:42:44 +0400

    nice

--- Module 4/Lesson 6/l6.py
+++ Module 4/Lesson 6/l6.py
@@ -1,6 +1,6 @@
 import requests
 from config import HF_API_KEY
-from colorama import Fore, Stylem, init
+from colorama import Fore, Style, init
 
 init(autoreset=True)
 DEFAULT_MODEL = "google/pegasus-xsum"
